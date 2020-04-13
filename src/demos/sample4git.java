d4718b99618af3e217df587ed157d935f0ba11f2
Indrajeet <tidke_indrajeet@hotmail.com>
2020-04-14 01:01:12 +0530

    to branch

--- src/demos/sample4git.java
+++ src/demos/sample4git.java
@@ -5,6 +5,10 @@ public class sample4git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample program for git");
+		System.out.println("sample program for git1");
+		
+		System.out.println("sample program for git2");
+		System.out.println("sample program for git3");
 
 	}
 
